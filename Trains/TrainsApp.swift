889f155df17c0cbefd973a89e11798e467820972
Sultan <sultanisabayev2@gmail.com>
2025-10-20 20:40:31 +0500

    Initial Commit

---
+++ Trains/TrainsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainsApp.swift
+//  Trains
+//
+//  Created by Sultan Akhmetbek on 20.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct TrainsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
